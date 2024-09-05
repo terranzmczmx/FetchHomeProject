9acbce9c2e1253a3ac7e707fe4a43b94ca0a8674
Wing Hu <39383762+terranzmczmx@users.noreply.github.com>
2024-09-04 18:48:30 -0700

    Initial Commit

---
+++ FetchHomeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchHomeProject
+//
+//  Created by Yingzhe Hu on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchHomeProject/FetchHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchHomeProjectApp.swift
+//  FetchHomeProject
+//
+//  Created by Yingzhe Hu on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
